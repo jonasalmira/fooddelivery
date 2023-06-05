2bb168a2b39d06c2e587eacceb58a56ae35cb9b0
jonasalmira <jonas.almira@developersinstitute.co.nz>
2023-06-05 18:03:42 +1200

    imported useNavigation, added useLayoutEffect and a SafeAreaView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Image, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className="text-orange-600">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-orange-600">
+        <View>
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
+      </Text>
+    </SafeAreaView>
   );
 };
 
