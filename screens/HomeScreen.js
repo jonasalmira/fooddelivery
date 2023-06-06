c09800a20b4d3a824e40ce6082cb95d590a7af4e
jonasalmira <jonas.almira@developersinstitute.co.nz>
2023-06-07 00:56:55 +1200

    implemented flex to the header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,8 +19,8 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-orange-600">
+    <SafeAreaView className="bg-white pt-5">
+      <View className="text-orange-600">
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
@@ -28,7 +28,7 @@ const HomeScreen = () => {
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-          <View>
+          <View className="flex-1">
             <Text className="font-bold text-gray-400 text-xs">
               Deliver Now!
             </Text>
@@ -37,8 +37,10 @@ const HomeScreen = () => {
               <ChevronDownIcon size={20} color="#00CCBB" />
             </Text>
           </View>
+
+          <UserIcon size={35} color="#00CCBB" />
         </View>
-      </Text>
+      </View>
     </SafeAreaView>
   );
 };
