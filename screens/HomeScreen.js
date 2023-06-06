e890af86bae1756836256c7ffcf17b32da10b425
jonasalmira <jonas.almira@developersinstitute.co.nz>
2023-06-07 00:06:21 +1200

    added classNames to the View and Text components in HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,13 +15,19 @@ const HomeScreen = () => {
   return (
     <SafeAreaView>
       <Text className="text-orange-600">
-        <View>
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text classNamr="font-bold text-xt">Current Location</Text>
+          </View>
         </View>
       </Text>
     </SafeAreaView>
