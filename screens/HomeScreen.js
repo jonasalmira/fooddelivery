82a9655c691992552e4699c12750650d60e8b0f2
jonasalmira <jonas.almira@developersinstitute.co.nz>
2023-06-08 01:57:28 +1200

    added a folder containing the Category and Card components. This category section has cards that can be scrolled horizontally

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Image, Text, TextInput } from "react-native";
+import { View, Image, Text, TextInput, ScrollView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,6 +8,7 @@ import {
   MagnifyingGlassIcon,
   AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -20,39 +21,42 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <View className="text-orange-600">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
 
-          <UserIcon size={35} color="#00CCBB" />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
 
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p3">
-            <MagnifyingGlassIcon color="#00CCBB" />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View>
-
-          <AdjustmentsVerticalIcon color="#00CCBB" />
-        </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p3">
+          <MagnifyingGlassIcon color="#00CCBB" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      <ScrollView>
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
